a8671663853a83590330c5452400989144777983
Sushant69005 <parasharsushant4@gmail.com>
2025-07-01 13:20:21 +0530

    Initial commit - Netflix Clone

---
+++ script.js
@@ -0,0 +1,161 @@
+const translations = {
+  English: {
+    title: "Unlimited movies,TV shows and more",
+    description: "Starts at ₹149. Cancel anytime.",
+    getStarted: "Get Started",
+    membership:
+      "Ready to watch? Enter your email to create or restart your membership.",
+    emailPlaceholder: "Email address",
+    trending: "Trending Now",
+    reasons: "More reasons to join",
+    watchontv: "Enjoy on your TV",
+    wheretowatch:
+      "Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.",
+    downloadtowatch: "Download your shows to watch offline",
+    favouriteshow:
+      "Save your favourites easily and always have something to watch.",
+    watcheverywhere: "Watch everywhere",
+    streamunlimited:
+      "Stream unlimited movies and TV shows on your phone, tablet, laptop and TV.",
+    profileforkids: "Create profiles for kids",
+    favourite:
+      "Send kids on adventures with their favourite characters in a space made just for them — free with your membership.",
+    frequentlyasked: "Frequently Asked Questions",
+    question1: "What is Netflix?",
+    question2: "How much does netflix cost?",
+    question3: "Where can i watch?",
+    question4: "How do i cancel?",
+    question5: "What can i watch on Netflix?",
+    question6: "Is Netflix good for kids?",
+    readytowatch:
+      "Ready to watch? Enter your email to create or restart your membership.",
+    helpline: "Questions? Call 000-800-919-1743",
+    investor: "Investor Relations",
+    privacy: "Privacy",
+    speedtest: "Speed Test",
+    helpcentre: "Help Centre",
+    jobs: "Jobs",
+    preferences: "Cookie Preferences",
+    legalnotice: "Legal Notices",
+    account: "Account",
+    ways: "Ways To Watch",
+    corporateinfo: "Corporate Information",
+    onlyonnetflix: "Only On Netflix",
+    mediacentre: "Media Centre",
+    termsofuse: "Terms Of Use",
+    contactus: "Contact Us",
+    netflixIndia: "Netflix India",
+
+    securedBy:
+      "This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more",
+
+      answer1: "Netflix is a streaming service with movies, TV shows, and documentaries across genres, available on any internet-connected device.",
+      answer2: "Watch Netflix on any device for a fixed monthly fee starting at ₹149, with no extra costs or contracts.",
+      answer3: "Watch Netflix anytime, anywhere on any internet-connected device or download shows on the app to watch offline while you're on the go.",
+      answer4: "Netflix is flexible, with no contracts or fees, letting you start or cancel your account anytime in just two clicks.",
+      answer5: "Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.",
+      answer6: "The Netflix Kids experience offers a PIN-protected, family-friendly space with parental controls to manage what kids can watch on your membership.",
+ 
+
+  
+  },
+
+  Hindi: {
+    title: "अनलिमिटेड फ़िल्में, टीवी शो, और बहुत कुछ",
+    description: "₹149 से शुरू होता है. कभी भी कैंसल करें.",
+    getStarted: "शुरू करें",
+    membership:
+      "देखने के लिए तैयार हैं? अपनी मेंबरशिप बनाने या रीस्टार्ट करने के लिए अपना ईमेल एड्रेस डालें.",
+      emailPlaceholder: "ईमेल पता",
+    trending: "ट्रेंडिंग नाव",
+    reasons: "शामिल होने की ज़्यादा वजहें",
+    watchontv: "अपने टीवी पर आनंद लें",
+    wheretowatch:
+      "स्मार्ट टीवी, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray प्लेयर के साथ ही दूसरे डिवाइस पर भी देखें.",
+    downloadtowatch: "ऑफ़लाइन देखने के लिए अपने शो डाउनलोड करें",
+    favouriteshow:
+      "अपने पसंदीदा शो और फ़िल्में सेव करें, ताकि आप कभी भी इन्हें देख सकें.",
+    watcheverywhere: "हर जगह देखें",
+    streamunlimited:
+      "बिना ज़्यादा पेमेंट किए, अपने फ़ोन, टैबलेट, लैपटॉप और टीवी पर अनलिमिटेड फ़िल्में और टीवी शो स्ट्रीम करें.",
+    profileforkids: "बच्चों के लिए प्रोफ़ाइल बनाएं",
+    favourite:
+      "बच्चों को अपने पसंदीदा किरदारों के साथ उस रोमांचक सफ़र पर जाने दें जो सिर्फ़ उनके लिए ही है - आपकी मेंबरशिप के साथ फ़्री.",
+    frequentlyasked: "अक्सर पूछे जाने वाले सवाल",
+    question1: "Netflix क्या है?",
+    question2: "Netflix की कीमत कितनी है?",
+    question3: "मैं कहां देख सकता हूं?",
+    question4: " मैं कैसे रद्द कर सकता हूं?",
+    question5: "Netflix पर क्या देखा जा सकता है?",
+    question6: "क्या Netflix बच्चों के लिए अच्छा है?",
+    readytowatch:
+      "देखने के लिए तैयार हैं? अपनी मेंबरशिप बनाने या रीस्टार्ट करने के लिए अपना ईमेल एड्रेस डालें.",
+    helpline: "कोई सवाल? 000-800-919-1743 पर कॉल करें",
+    investor: "इंवेस्टर संबंध",
+    privacy: "प्रायवेसी",
+    speedtest: "स्पीड टेस्ट",
+    helpcentre: "सहायता केंद्र",
+    jobs: "नौकरियां",
+    preferences: "कुकी प्रेफ़रेंस",
+    legalnotice: "कानूनी सूचनाएं",
+
+    account: "अकाउंट",
+    ways: "देखने के तरीके",
+    corporateinfo: "कॉरपोरेट जानकारी",
+    onlyonnetflix: "सिर्फ़ Netflix पर",
+
+    mediacentre: "मीडिया केंद्र",
+    termsofuse: "उपयोग की शर्तें",
+    contactus: "हमसे संपर्क करें",
+    netflixIndia: "Netflix भारत",
+    securedBy:
+      "यह पक्का करने के लिए कि आप बॉट नहीं हैं, यह पेज Google reCAPTCHA द्वारा सुरक्षित है. और जानें",
+      answer1: "Netflix एक स्ट्रीमिंग सर्विस है जिसके ज़रिए आप हज़ारों इंटरनेट-कनेक्टेड डिवाइस पर तरह-तरह के अवॉर्ड विजेता टीवी शो, फ़िल्में, ऐनिमे, डॉक्यूमेंट्रीज़ का लुत्फ़ उठा सकते हैं.",
+      answer2: "हर महीने की तय फ़ी देकर अपने स्मार्टफ़ोन, टैबलेट, स्मार्ट टीवी, लैपटॉप, या स्ट्रीमिंग डिवाइस पर Netflix देखें. हर महीने ₹149 से ₹649 तक के प्लान. कोई एक्सट्रा कीमत या कॉन्ट्रैक्ट नहीं.",
+      answer3: "कहीं भी, कभी भी देखें. तुरंत देखने के लिए वेब पर netflix.com पर जाकर अपने Netflix अकाउंट से साइन इन करें",
+      answer4: "Netflix लचीला है. कोई परेशान करने वाले कॉन्ट्रैक्ट नहीं और कोई बंधन नहीं हैं. आप आसानी से दो क्लिक में अपना अकाउंट ऑनलाइन कैंसल कर सकते हैं. कोई कैंसलेशन फ़ीस नहीं है – अपना अकाउंट कभी भी शुरू या बंद करें.",
+      answer5: "Netflix की बहुत बड़ी लाइब्रेरी में फ़ीचर फ़िल्मों, डॉक्यूमेंट्री, टीवी शो, ऐनिमे, अवॉर्ड-विनिंग Netflix ओरिजिनल्स के अलावा और भी बहुत कुछ है. आप जितना चाहें, कभी भी देखें.",
+      answer6: "आपकी मेंबरशिप में Netflix किड्स एक्सपीरियंस शामिल है.बच्चों की प्रोफ़ाइल में PIN से सुरक्षित पैरेंटल कंट्रोल्स होते हैं जिससे आप  उन टाइटल को ब्लॉक कर सकते हैं जिन्हें आप नहीं चाहते हैं कि बच्चे देखें.",
+
+
+
+  },
+};
+
+const selectlanguage = document.getElementById("selectlanguage");
+
+const savedLang = localStorage.getItem("language") || "English";
+selectlanguage.value = savedLang;
+setLanguage(savedLang);
+
+selectlanguage.addEventListener("change", function () {
+  const selectedLang = this.value;
+  localStorage.setItem("language", selectedLang);
+  setLanguage(selectedLang);
+});
+
+function setLanguage(lang) {
+  document.querySelectorAll("[data-key]").forEach((element) => {
+    const key = element.getAttribute("data-key");
+    element.textContent = translations[lang][key];
+  });
+
+  document.querySelectorAll("[data-placeholder-key]").forEach(element => {
+    const key = element.getAttribute("data-placeholder-key");
+    element.placeholder = translations[lang][key];
+  });
+}
+
+
+
+
+document.querySelectorAll('.askedquestions').forEach(question => {
+    question.addEventListener('click', () => {
+        const answer = question.nextElementSibling;
+        const icon = question.querySelector('.plusicon');
+
+        answer.classList.toggle('active');
+
+        icon.classList.toggle('rotate');
+    });
+});
